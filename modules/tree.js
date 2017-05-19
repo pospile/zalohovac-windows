9b7c0920c298109bfbcb5c3eef919c329b78bc68
Pospichal <VPO@HD.HUD>
2017-05-19 17:28:35 +0200

    Better tree module

--- modules/tree.js
+++ modules/tree.js
@@ -1,16 +1,64 @@
-var dirToJson = require('dir-to-json');
-const util = require('util');
-var jsonfile = require('jsonfile');
+var fs = require('fs'),
+    path = require('path');
+ 
+function dirTree(filename, depth) {
+    var stats, info;
+    try 
+    {
+        stats = fs.lstatSync(filename);
+        info = {
+                path: filename,
+                name: path.basename(filename)
+            };
 
-
+        if (stats.isDirectory()) {
+            if (depth > 5)
+            {
+                info.type = "folder";
+                info.children = "...";
+            }
+            else
+            {
+                info.type = "folder";
+                info.children = fs.readdirSync(filename).map(function(child) {
+                    if (child != "Windows" && child != "PerfLogs")
+                    {
+                        if (child == "Users") {depth = 0;}
+                        return dirTree(filename + '/' + child, depth+1);
+                    }
+                    else
+                    {
+                        return "System folder, backup unavailable";
+                    }
+                });
+            }
+        } else {
+            // Assuming it's a file. In real life it could be a symlink or
+            // something else!
+            info.type = "file";
+            info.size = getFilesizeInBytes(info.path);
+        }
+    }
+    catch(err)
+    {
+        info = {
+                path: "error",
+                name: "error-file-locked-or-busy",
+                log: err
+            };
+    }
+    
+    return info;
+}
+ 
 if (module.parent == undefined) {
-    dirToJson( "C:\\" )
-        .then( function( dirTree ){
-            //console.log( util.inspect(dirTree, false, null)  );
-            jsonfile.writeFileSync("./tree.json", dirTree);
-            //console.log(jsonfile.readFileSync("./tree.json"));
-        })
-        .catch( function( err ){
-            throw err;
-        });
+    // node dirTree.js ~/foo/bar
+    var util = require('util');
+    console.log(util.inspect(dirTree("C:/",0), false, null));
+}
+
+function getFilesizeInBytes(filename) {
+    const stats = fs.statSync(filename)
+    const fileSizeInBytes = stats.size
+    return fileSizeInBytes
 }
